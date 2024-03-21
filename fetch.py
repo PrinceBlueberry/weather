a63894ea21e0948d3cba3eee3220d3055d1ce58d
owencail <owenc@elmersmfg.com>
2024-03-21 15:05:09 -0500

    Move more things out of main

--- fetch.py
+++ fetch.py
@@ -6,6 +6,10 @@ import requests
 base_url = "https://dd.weather.gc.ca"
 cache_path = "./cache"
 
+rel_path_station_list = "./climate/observations/climate_station_list.csv"
+
+provincial_code_lookup = {"MANITOBA": "MB"}
+
 
 def _file(file_url, file_path):
     """Go to the web-accessible-file and download it into our cache."""
@@ -19,8 +23,8 @@ def _file(file_url, file_path):
         raise FileNotFoundError(f"unable to download file, got code: {response.status_code}")
 
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    with open(file_path, "wb") as file:
-        file.write(file_contents)
+    with open(file_path, "wb") as f:
+        f.write(file_contents)
     print(f"downloaded and saved file: {file_path}")
 
 
@@ -39,3 +43,62 @@ def file(relative_path) -> pandas.DataFrame:
         return pandas.DataFrame()
     else:
         return pandas.read_csv(path_to_cache, parse_dates=[4], encoding="cp1252")
+
+
+def station_list():
+    return file(rel_path_station_list)
+
+
+def all_daily_for(climate_id):
+    stations = file(rel_path_station_list)
+    this_station_mask = stations.loc[:, "Climate ID"] == climate_id
+    if this_station_mask.sum() != 1:
+        raise ValueError(f"Expected 1 station for given ID, found {this_station_mask.sum()}")
+    province_name = stations.loc[this_station_mask, "Province"].values[0]
+    province = provincial_code_lookup[province_name]
+    first_year = int(stations.loc[this_station_mask, "DLY First Year"].values[0])
+    last_year = int(stations.loc[this_station_mask, "DLY Last Year"].values[0])
+    df = pandas.DataFrame()
+    for year in range(first_year, last_year + 1):
+        for month in range(1, 13):
+            path = path_for_daily_data(
+                climate_id=climate_id, year=year, month=month, province=province
+            )
+            try:
+                new_df = file(path)
+                if not new_df.empty:
+                    df = pandas.concat([df, new_df])
+            except FileNotFoundError:
+                # Even though we know which years should exist on the server, we don't know that
+                # all months of each year have data.
+                print(f"WARNING: no daily data for station {climate_id} on {year}-{month}")
+    return df
+
+
+def all_hourly_for(climate_id):
+    stations = station_list()
+    this_station_mask = stations.loc[:, "Climate ID"] == climate_id
+    if this_station_mask.sum() != 1:
+        raise ValueError(f"Expected 1 station for given ID, found {this_station_mask.sum()}")
+    province_name = stations.loc[this_station_mask, "Province"].values[0]
+    province = provincial_code_lookup[province_name]
+    first_year = int(stations.loc[this_station_mask, "HLY First Year"].values[0])
+    last_year = int(stations.loc[this_station_mask, "HLY Last Year"].values[0])
+    df = pandas.DataFrame()
+    for year in range(first_year, last_year):
+        try:
+            path = path_for_hourly_data(climate_id=climate_id, year=year, province=province)
+            new_df = file(path)
+            if not new_df.empty:
+                df = pandas.concat([df, new_df])
+        except FileNotFoundError:
+            print(f"WARNING: no hourly data for station {climate_id} on {year}")
+    return df
+
+
+def path_for_daily_data(climate_id, year, month, province):
+    return f"./climate/observations/daily/csv/{province}/climate_daily_{province}_{climate_id}_{year}-{month:02d}_P1D.csv"
+
+
+def path_for_hourly_data(climate_id, year, province):
+    return f"./climate/observations/hourly/csv/{province}/climate_hourly_{province}_{climate_id}_{year}_P1H.csv"
--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import pandas
+import plotly.graph_objects as go
 
 import fetch
-
-rel_path_station_list = "./climate/observations/climate_station_list.csv"
-
-provincial_code_lookup = {"MANITOBA": "MB"}
-
-
-def generate_path_for_daily_data(climate_id, year, month, province):
-    return f"./climate/observations/daily/csv/{province}/climate_daily_{province}_{climate_id}_{year}-{month:02d}_P1D.csv"
-
-
-def generate_path_for_hourly_data(climate_id, year, province):
-    return f"./climate/observations/hourly/csv/{province}/climate_hourly_{province}_{climate_id}_{year}_P1H.csv"
 
 
 def show_stations_with_the_most_history(count=5, province="MANITOBA"):
@@ -22,7 +10,7 @@ def show_stations_with_the_most_history(count=5, province="MANITOBA"):
         "HLY_Interval",
         "DLY_Interval",
     ]
-    sl = fetch.file(rel_path_station_list)
+    sl = fetch.station_list()
     sl.loc[:, "HLY_Interval"] = sl.loc[:, "HLY Last Year"] - sl.loc[:, "HLY First Year"]
     sl.loc[:, "DLY_Interval"] = sl.loc[:, "DLY Last Year"] - sl.loc[:, "DLY First Year"]
     sl = sl[sl.Province == province]
@@ -42,69 +30,18 @@ def show_stations_with_the_most_history(count=5, province="MANITOBA"):
     print()
 
 
-def get_all_hourly_for(climate_id):
-    stations = fetch.file(rel_path_station_list)
-    this_station_mask = stations.loc[:, "Climate ID"] == climate_id
-    if this_station_mask.sum() != 1:
-        raise ValueError(f"Expected 1 station for given ID, found {this_station_mask.sum()}")
-    province_name = stations.loc[this_station_mask, "Province"].values[0]
-    province = provincial_code_lookup[province_name]
-    first_year = int(stations.loc[this_station_mask, "HLY First Year"].values[0])
-    last_year = int(stations.loc[this_station_mask, "HLY Last Year"].values[0])
-    df = pandas.DataFrame()
-    for year in range(first_year, last_year):
-        try:
-            path = generate_path_for_hourly_data(
-                climate_id=climate_id, year=year, province=province
-            )
-            new_df = fetch.file(path)
-            if not new_df.empty:
-                df = pandas.concat([df, new_df])
-        except FileNotFoundError:
-            print(f"WARNING: no hourly data for station {climate_id} on {year}")
-    return df
-
-
-def get_all_daily_for(climate_id):
-    stations = fetch.file(rel_path_station_list)
-    this_station_mask = stations.loc[:, "Climate ID"] == climate_id
-    if this_station_mask.sum() != 1:
-        raise ValueError(f"Expected 1 station for given ID, found {this_station_mask.sum()}")
-    province_name = stations.loc[this_station_mask, "Province"].values[0]
-    province = provincial_code_lookup[province_name]
-    first_year = int(stations.loc[this_station_mask, "DLY First Year"].values[0])
-    last_year = int(stations.loc[this_station_mask, "DLY Last Year"].values[0])
-    df = pandas.DataFrame()
-    for year in range(first_year, last_year + 1):
-        for month in range(1, 13):
-            path = generate_path_for_daily_data(
-                climate_id=climate_id, year=year, month=month, province=province
-            )
-            try:
-                new_df = fetch.file(path)
-                if not new_df.empty:
-                    df = pandas.concat([df, new_df])
-            except FileNotFoundError:
-                # Even though we know which years should exist on the server, we don't know that
-                # all months of each year have data.
-                print(f"WARNING: no daily data for station {climate_id} on {year}-{month}")
-    return df
-
-
 my_climate_ids = {
     "Gretna": "5021220",
     "Pilot Mound": "5022125",
 }
+temperature_threshold = -10  # [C]
 
 if __name__ == "__main__":
     print("Starting")
     show_stations_with_the_most_history()
-    rel_path_daily = generate_path_for_daily_data(
-        climate_id=my_climate_ids["Gretna"], year=2023, month=12, province="MB"
-    )
     print("Getting all weather data...")
-    daily = get_all_daily_for(my_climate_ids["Pilot Mound"])
-    hourly = get_all_hourly_for(my_climate_ids["Pilot Mound"])
+    daily = fetch.all_daily_for(my_climate_ids["Pilot Mound"])
+    hourly = fetch.all_hourly_for(my_climate_ids["Pilot Mound"])
     print(hourly)
     print(daily)
     print()
